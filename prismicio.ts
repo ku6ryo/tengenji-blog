090b644ee5685e938160ccc59ef7f00a69e94cf6
ku6ryo <ku6ryo@gmail.com>
2022-05-01 23:26:33 +0900

    removed unnecessary

--- prismicio.ts
+++ prismicio.ts
@@ -1,20 +1,7 @@
 import * as prismic from '@prismicio/client'
-import { LinkResolverFunction } from '@prismicio/helpers'
 
 export const endpoint = process.env.PRISMIC_API_ENDPOINT || ""
 export const repositoryName = prismic.getRepositoryName(endpoint)
-
-// Update the Link Resolver to match your project's route structure
-export const linkResolver: LinkResolverFunction = (doc) => {
-  switch (doc.type) {
-    case 'homepage':
-      return '/'
-    case 'page':
-      return `/${doc.uid}`
-    default:
-      return '/'
-  }
-}
 
 export const client = prismic.createClient(repositoryName, {
   accessToken: "",
